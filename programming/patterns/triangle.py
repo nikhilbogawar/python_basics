4d6cb0da26774113d87ceb798bd04fafd128c1fd
nikhilbogawar <tejasnikhil72@gmail.com>
2026-07-07 21:49:14 +0530

    edited something in triangles.py file

--- programming/patterns/triangle.py
+++ programming/patterns/triangle.py
@@ -1,51 +1,67 @@
 n=int(input())
-# for i in range(1,n+1):
-#     for j in range(1,n+1):
-#         if i<=j:
-#             print("*",end=" ")
-#     print()
-# # * * * * * 
-# # * * * * 
-# # * * * 
-# # * * 
-# # * 
-# print("------------------------------------")
-# for i in range(1,n+1):
-#     for j in range(1,n+1):
-#         if i>=j:
-#             print("*",end=" ")
-#     print()
-# # * 
-# # * * 
-# # * * * 
-# # * * * * 
-# # * * * * * 
-# print("------------------------------------")
-# for i in range(1,n+1):
-#     for j in range(1,n+1):
-#         if i<=j:
-#             print("*",end=" ")
-#         else:
-#             print(" ",end=" ")
-#     print()
-# # * * * * * 
-# #   * * * * 
-# #     * * * 
-# #       * * 
-# #         * 
-# print("------------------------------------")
-# for i in range(1,n+1):
-#     for j in range(1,n+1):
-#         if i+j>=n+1:
-#             print("*",end=" ")
-#         else:
-#             print(" ",end=" ")
-#     print()
-# #         * 
-# #       * * 
-# #     * * * 
-# #   * * * * 
-# # * * * * *
+for i in range(1,n+1):
+    # for j in range(1,n+1):
+    #     if i<=j:
+    #         print("*",end=" ")
+    # print()
+    for j in range(1,n-i+2):
+        print(end="*")
+    print()
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+print("------------------------------------")
+for i in range(1,n+1):
+    # for j in range(1,n+1):
+    #     if i>=j:
+    #         print("*",end=" ")
+    # print()
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+print("------------------------------------")
+for i in range(1,n+1):
+    # for j in range(1,n+1):
+    #     if i<=j:
+    #         print("*",end=" ")
+    #     else:
+    #         print(" ",end=" ")
+    # print()
+    for j in range(1,i):
+        print(end=" ")
+    for j in range(1,n-i+2):
+        print(end="*")
+    print()
+# * * * * * 
+#   * * * * 
+#     * * * 
+#       * * 
+#         * 
+print("------------------------------------")
+for i in range(1,n+1):
+    # for j in range(1,n+1):
+    #     if i+j>=n+1:
+    #         print("*",end=" ")
+    #     else:
+    #         print(" ",end=" ")
+    # print()
+    for j in range(1,n-i+1):
+        print(end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+# * * * * *
 print("------------------------------------")
 c=65
 for i in range(1,n+1):
