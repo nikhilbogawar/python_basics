62bd6f846609d5cdde433512875990874a070399
nikhilbogawar <tejasnikhil72@gmail.com>
2026-07-24 20:14:53 +0530

    me and my brother name using hallow patterns code in special_one.py

--- programming/patterns/special_one.py
+++ programming/patterns/special_one.py
@@ -1,49 +1,87 @@
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if j==1 or i==1 or i==n:
+        if i==1 or j==n//2+1:
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
     for j in range(1,n+1):
-        if j==1 or j==n or i==n//2+1:
+        if i==1 or i==n or i==n//2+1 or j==1:
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
     for j in range(1,n+1):
-        if j==1 or i==1 or j==n or i==n//2+1:
+        if i==1 or j==n//2+1 or (i==n and j<=n//2+1):
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
+    for j in range(1,n+1):
+        if i==1 or i==n//2+1 or j==1 or j==n:
+            print("*",end=" ")
+        else:
+            print(end="  ")
+    print(end="  ")
+    for j in range(1,n+1):
+        if i==1 or i==n or i==j:
+            print("*",end=" ")
+        else:
+            print(end="  ")
+    print(end="  ")
+    print()
+print()
+print()
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j==1 or j==n or i==j:
+            print("*",end=" ")
+        else:
+            print(end="  ")
+    print(end="  ")
     for j in range(1,n+1):
         if i==1 or i==n or j==n//2+1:
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
     for j in range(1,n+1):
-        if i==1 or j==n//2+1:
+        if j==1 or (i+j==n+1 and i<=n//2+1) or (i==j and i>n//2):
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
     for j in range(1,n+1):
-        if j==1 or j==n or i==n:
+        if j==1 or j==n or i==n//2+1:
             print("*",end=" ")
         else:
             print(end="  ")
-    print(" ",end="")
+    print(end="  ")
     for j in range(1,n+1):
-        if j==1 or j==n or i==n:
+        if i==1 or i==n or j==n//2+1:
             print("*",end=" ")
         else:
             print(end="  ")
+    print(end="  ")
+    for j in range(1,n+1):
+        if j==1 or i==n:
+            print("*",end=" ")
+        else:
+            print(end="  ")
+    print(end="  ")
     print()
-# * * * * *  *       *  * * * * *  * * * * *  * * * * *  *       *  *       * 
-# *          *       *  *       *      *          *      *       *  *       * 
-# *          * * * * *  * * * * *      *          *      *       *  *       * 
-# *          *       *  *       *      *          *      *       *  *       * 
-# * * * * *  *       *  *       *  * * * * *      *      * * * * *  * * * * * 
+    
+    
+# * * * * *   * * * * *   * * * * *   * * * * *   * * * * *   
+#     *       *               *       *       *     *         
+#     *       * * * * *       *       * * * * *       *       
+#     *       *               *       *       *         *     
+#     *       * * * * *   * * *       *       *   * * * * *   
+
+
+# *       *   * * * * *   *       *   *       *   * * * * *   *           
+# * *     *       *       *     *     *       *       *       *           
+# *   *   *       *       *   *       * * * * *       *       *           
+# *     * *       *       *     *     *       *       *       *           
+# *       *   * * * * *   *       *   *       *   * * * * *   * * * * *   
